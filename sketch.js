48f1f245e92c858057d2f16008abbd568a7a2396
skippywalks <32936936+skippywalks@users.noreply.github.com>
2023-07-31 12:51:06 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,11 +13,13 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  
   for (let i = 0; i < numOfChickens; i++) {
     chickens[i] = new Chicken(width / 4, height - 400 - i * 50);
   }
+  
   dartboard = new Dartboard(width - 200, 200, dartboardRadius);
-
+  
   let resetButton = select('#resetButton');
   resetButton.mousePressed(resetGame);
 }
@@ -29,11 +31,14 @@ function draw() {
     chicken.update();
     chicken.display();
   }
+
+  fill(255);
+  textSize(16);
 }
 
 function mousePressed() {
   for (let chicken of chickens) {
-    if (dist(mouseX, mouseY, chicken.x, chicken.y) < 50 && !chicken.isFlying) {
+    if (dist(mouseX, mouseY, chicken.x, chicken.y) < 50 && !chicken.isFlying && !chicken.landed) {
       draggingChicken = chicken;
     }
   }
@@ -57,7 +62,6 @@ function resetGame() {
   for (let i = 0; i < numOfChickens; i++) {
     chickens[i] = new Chicken(width / 4, height - 400 - i * 50);
   }
-  loop();
 }
 
 class Chicken {
@@ -72,27 +76,35 @@ class Chicken {
     this.offsetY = 0;
     this.vx = 0;
     this.vy = 0;
-    this.strength = 0.3; // this controls the strength of the throw
-    this.maxRange = 200; // Maximum effective pull back distance
+    this.strength = 0.3; 
+    this.maxRange = 200; 
+    this.landed = false;
   }
 
   display() {
-  // Draw pull back line
-  if (this.dragging) {
-    let pullBackDist = dist(this.startX, this.startY, this.x, this.y);
-    let lineColor = color(0, 255, 0); // Start with green color for line
-    if (pullBackDist > this.maxRange) {
-      lineColor = color(255, 0, 0); // If pull back is too far, make the line red
+    if (this.dragging) {
+      let pullBackDist = dist(this.startX, this.startY, this.x, this.y);
+      let lineColor = color(0, 255, 0);
+      if (pullBackDist > this.maxRange) {
+        lineColor = color(255, 0, 0);
+      }
+      stroke(lineColor);
+      line(this.startX, this.startY, this.x, this.y);
     }
-    stroke(lineColor);
-    line(this.startX, this.startY, this.x, this.y);
+    image(chickenImg, this.x, this.y, 100, 100);
+    if (this.landed) {
+      fill(255);
+      textSize(16);
+      try {
+        text(this.score, this.x, this.y);
+      } catch (err) {
+        console.log('Error displaying score:', err);
+      }
+    }
   }
-  // Draw the chicken
-  image(chickenImg, this.x, this.y, 100, 100);
-}
 
   drag(mx, my) {
-    if (this.isFlying) return;
+    if (this.isFlying || this.landed) return;
     let d = dist(mx, my, this.x, this.y);
     if (d < 50) {
       this.dragging = true;
@@ -103,32 +115,25 @@ class Chicken {
 
   update() {
     if (this.dragging) {
-      let newPosX = mouseX + this.offsetX;
-      let newPosY = mouseY + this.offsetY;
-      let pullBackDist = dist(this.startX, this.startY, newPosX, newPosY);
-
-      if (pullBackDist <= this.maxRange) {
-        this.x = newPosX;
-        this.y = newPosY;
-      } else {
-        // Calculate the angle of pull back
-        let pullBackAngle = atan2(this.startY - newPosY, this.startX - newPosX);
-        // Update the chicken's position to the maximum range in the same direction of pull back
-        this.x = this.startX - this.maxRange * cos(pullBackAngle);
-        this.y = this.startY - this.maxRange * sin(pullBackAngle);
-      }
-    } else if (this.isFlying) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    } else if (this.isFlying && !this.landed) {
       this.x += this.vx;
       this.y += this.vy;
       this.vy += 0.25; // gravity
       if (dartboard.contains(this.x, this.y)) {
-        this.isFlying = false;
+        this.landed = true;
+        this.vx = 0; // stop horizontal movement
+        this.vy = 0; // stop vertical movement
+        this.score = dartboard.getScore(this.x, this.y);
+        console.log(`Chicken landed in: ${this.score}`);
       }
     }
   }
 
+
   fly() {
-    if (this.dragging) {
+    if (this.dragging && !this.landed) {
       this.isFlying = true;
       this.vx = (this.startX - this.x) * this.strength;
       this.vy = (this.startY - this.y) * this.strength;
@@ -137,17 +142,44 @@ class Chicken {
   }
 }
 
-
 class Dartboard {
   constructor(x, y, r) {
     this.x = x;
     this.y = y;
     this.r = r;
+    this.scores = [6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10];
   }
 
   contains(x, y) {
+    return dist(this.x, this.y, x, y) < this.r;
+  }
+
+  getScore(x, y) {
     let d = dist(this.x, this.y, x, y);
-    return d < this.r;
+    let angle = atan2(y - this.y, x - this.x);
+    if (angle < 0) {
+      angle += TWO_PI;
+    }
+    let sector = floor(angle / (TWO_PI / 20));
+
+    let normalizedD = d / this.r;
+    let multiplier;
+
+    if (normalizedD <= 4 / this.r) {
+      multiplier = 50;
+    } else if (normalizedD <= 8 / this.r) {
+      multiplier = 25;
+    } else if (normalizedD <= 31.8 / this.r) {
+      multiplier = 2;
+    } else if (normalizedD <= 81 / this.r) {
+      multiplier = 1;
+    } else if (normalizedD <= 107 / this.r) {
+      multiplier = 3;
+    } else {
+      multiplier = 1;
+    }
+
+    let baseScore = this.scores[sector];
+    return baseScore * multiplier;
   }
 }
-
